2518dbc9dde39b58bf2a6d33975960059f39e3b0
Sebastian Vetter <sebastian@roadside-developer.com>
2014-01-13 12:18:46 +1100

    Fix issue with template rendering

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='{{ cookiecutter.repo_name }}',
     version="0.0.0",
-    url='https://github.com/{{ cookiecutter.github_user }}/{{ cookiecutter.app_name',
+    url='https://github.com/{{ cookiecutter.github_user }}/{{ cookiecutter.app_name }}',
     author="{{ cookiecutter.author_name }}",
     author_email="{{ cookiecutter.email }}",
     description="{{ cookiecutter.description }}",
